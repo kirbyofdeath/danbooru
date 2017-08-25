e5c965d89ab5ae337baf7dd7f3df4a37f0183ccc
root <root@kirbyserver.ddns.net>
2017-08-25 13:55:10 -0500

    changed server, users

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,5 @@
-set :user, "albert"
+set :user, "kirby"
 set :rails_env, "production"
-server "sonohara.donmai.us", :roles => %w(web app db), :primary => true, :user => "albert"
-server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
+server "kirbyboard.ddns.net", :roles => %w(web app db), :primary => true, :user => "kirby"
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
